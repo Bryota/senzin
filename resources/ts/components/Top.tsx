e03fb5dca544c4f5660ab245e38f0b63126e9bbc
Ryota Segawa <sryotapersian@gmail.com>
2021-04-07 12:25:43 +0000

    markup toppage

--- resources/ts/components/Top.tsx
+++ resources/ts/components/Top.tsx
@@ -5,47 +5,89 @@ const Top:React.FC = () => {
     return (
         <>
             <Header />
-            <div className="top__bgimg">
-                <h1 className="top__bgimg__title">先人の知恵</h1>
+            <div className="top-bgimg">
+                <h1 className="top-bgimg__title">先人の知恵</h1>
             </div>
-            <div className="top__ideas">
+            <div className="top-ideas">
                 <div className="container">
-                    <div className="row top__ideas__items">
-                        <div className="top__ideas__item col-4">
-                            <div className="top__ideas__item__balloon">
-                                <p className="top__ideas__item__title">テスト投稿</p>
-                                <p className="top__ideas__item__category">食べ物</p>
-                                <p className="top__ideas__item__content">てすとてすとてすとてすとてすとてすとてすとてすとてすとてすとてすとてすと・・・</p>
+                    <div className="row top-ideas__items">
+                        <div className="top-ideas__item col-4">
+                            <div className="top-ideas__item--balloon">
+                                <p className="top-ideas__item--title">テスト投稿</p>
+                                <p className="top-ideas__item--category">食べ物</p>
+                                <p className="top-ideas__item--content">てすとてすとてすとてすとてすとてすとてすとてすとてすとてすとてすとてすと・・・</p>
                             </div>
                             <div>
-                                <i className="far fa-user fa-5x top__ideas__item__icon"></i>
+                                <i className="far fa-user fa-5x top-ideas__item--icon"></i>
                             </div>
-                            <p className="top__ideas__item__username">テストユーザー</p>
+                            <p className="top-ideas__item--username">テストユーザー</p>
                         </div>
-                        <div className="top__ideas__item col-4">
-                            <div className="top__ideas__item__balloon">
-                                <p className="top__ideas__item__title">テスト投稿</p>
-                                <p className="top__ideas__item__category">食べ物</p>
-                                <p className="top__ideas__item__content">てすとてすとてすとてすとてすとてすとてすとてすとてすとてすとてすとてすと・・・</p>
+                        <div className="top-ideas__item col-4">
+                            <div className="top-ideas__item--balloon">
+                                <p className="top-ideas__item--title">テスト投稿</p>
+                                <p className="top-ideas__item--category">食べ物</p>
+                                <p className="top-ideas__item--content">てすとてすとてすとてすとてすとてすとてすとてすとてすとてすとてすとてすと・・・</p>
                             </div>
                             <div>
-                                <i className="far fa-user fa-5x top__ideas__item__icon"></i>
+                                <i className="far fa-user fa-5x top-ideas__item--icon"></i>
                             </div>
-                            <p className="top__ideas__item__username">テストユーザー</p>
+                            <p className="top-ideas__item--username">テストユーザー</p>
                         </div>
-                        <div className="top__ideas__item col-4">
-                            <div className="top__ideas__item__balloon">
-                                <p className="top__ideas__item__title">テスト投稿</p>
-                                <p className="top__ideas__item__category">食べ物</p>
-                                <p className="top__ideas__item__content">てすとてすとてすとてすとてすとてすとてすとてすとてすとてすとてすとてすと・・・</p>
+                        <div className="top-ideas__item col-4">
+                            <div className="top-ideas__item--balloon">
+                                <p className="top-ideas__item--title">テスト投稿</p>
+                                <p className="top-ideas__item--category">食べ物</p>
+                                <p className="top-ideas__item--content">てすとてすとてすとてすとてすとてすとてすとてすとてすとてすとてすとてすと・・・</p>
                             </div>
                             <div>
-                                <i className="far fa-user fa-5x top__ideas__item__icon"></i>
+                                <i className="far fa-user fa-5x top-ideas__item--icon"></i>
                             </div>
-                            <p className="top__ideas__item__username">テストユーザー</p>
+                            <p className="top-ideas__item--username">テストユーザー</p>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="top-category">
+                <div className="container">
+                    <h2 className="top-category__title">＜カテゴリ＞</h2>
+                    <div className="top-category__items">
+                        <div className="row justify-content-between">
+                            <div className="col-2 top-category__item">
+                                <p className="top-category__icon--food"><i className="fas fa-utensils"></i></p>
+                                <p className="top-category__name">食べ物</p>
+                            </div>
+                            <div className="col-2 top-category__item">
+                                <p className="top-category__icon--clean"><i className="fas fa-broom"></i></p>
+                                <p className="top-category__name">掃除</p>
+                            </div>
+                            <div className="col-2 top-category__item">
+                                <p className="top-category__icon--health"><i className="fas fa-plus-square"></i></p>
+                                <p className="top-category__name">健康</p>
+                            </div>
+                        </div>
+                        <div className="row justify-content-between">
+                            <div className="col-2 top-category__item">
+                                <p className="top-category__icon--sport"><i className="fas fa-running"></i></p>
+                                <p className="top-category__name">スポーツ</p>
+                            </div>
+                            <div className="col-2 top-category__item">
+                                <p className="top-category__icon--machine"><i className="fas fa-tv"></i></p>
+                                <p className="top-category__name">機械</p>
+                            </div>
+                            <div className="col-2 top-category__item">
+                                <p className="top-category__icon--other"><i className="fas fa-ellipsis-h"></i></p>
+                                <p className="top-category__name">その他</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="top-use">
+                <h2 className="top-use__title">＜使い方＞</h2>
+                <p className="top-use__content">先人の知恵として、皆さんが様々なアイデア・豆知識を投稿していくサービスです。</p>
+                <p className="top-use__content">１.投稿されているアイデア・豆知識を見てみましょう！<br/>普段の生活で使えるものが見つかるかも知れません。</p>
+                <p className="top-use__content">２.投稿してみましょう！<br/>あなたの投稿が誰かの生活の助けになるかも知れません。</p>
+                <p className="top-use__content">３.アカウントを作って、マイリストを作りましょう！<br/></p>
             </div>
         </>
     )
