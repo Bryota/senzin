0df60885fa7788b94674555c30f2470ae40178dd
Ryota Segawa <sryotapersian@gmail.com>
2021-04-20 13:05:06 +0000

    20210420/fix router

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('/getPostData', 'PostController@getPostData');
 Route::post('/setPostData', 'PostController@setPostData');
