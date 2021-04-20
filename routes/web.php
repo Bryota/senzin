6e26d9e3535b810e80948b8c9b4657ddac0afd8a
Ryota Segawa <sryotapersian@gmail.com>
2021-04-20 13:13:20 +0000

    20210420/fix router

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/{any}', function(){
-    return view('app');
-})->where('any', '.*');
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}','HomeController@app')->where('any', '.*');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
